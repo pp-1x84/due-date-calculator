b9c77f0b205c1e367579ef85e9c69eb1c0a217e1
pp-1x84 <j-alan@protonmail.com>
2025-04-26 11:31:19 -0400

    Initialize java/maven project

---
+++ src/main/java/com/emarsys/interview/duedatecalculator/DueDateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.emarsys.interview.duedatecalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class DueDateCalculatorApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
